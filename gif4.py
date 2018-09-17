51ef8ab389e53af989b293bcdf524426b36d5904
Sohail Siadatnejad <sohale@gmail.com>
2018-09-17 23:12:35 +0100

    some realistic movement with meaningful functions

--- gif4.py
+++ gif4.py
@@ -3,6 +3,7 @@ import imageio
 # https://imageio.github.io/
 
 import numpy as np
+import math
 
 """
 with imageio.get_writer('./anim1.gif', mode='I') as writer:
@@ -22,12 +23,39 @@ with imageio.get_writer('./anim1.gif', mode='I') as writer:
         writer.append_data(image[i])
 """
 
-nframes = 15
-image = np.zeros((nframes, 100,100,3), dtype=np.uint8)
-for i in range(nframes):
-    image[i, 10:20+i,10:20+i,1] = 120
+#nframes = 15
+dt = 0.02
+cycles = 1.0
+nframes = int(cycles/dt + 0.00000001)
+print(nframes, "frames")
+nx, ny = (100, 100)
+x0 = np.linspace(0, 1, nx)
+y0 = np.linspace(0, 1, ny)
 
-imageio.mimsave('./anim1.gif', image, duration=0.02)
+xx,yy = np.meshgrid(x0, y0)
+image = np.zeros((nframes, nx,ny,3), dtype=np.uint8)
+for i in range(nframes):
+    #image[i, 10:20+i,10:20+i,1] = 120
+    #image[i, 10:20+i,10:20+i,1] = 120
+    for rgbi in range(3):
+        t = i*dt
+        tb = int(i*dt)
+        tr = (i*dt) % 1.0
+        t010 = 1.0 - math.fabs(1.0-t*2.0)
+
+
+        sp_frq = 3.0
+
+        RADIANS = np.pi * 2
+        phi = t010 * RADIANS * 0.3
+
+        v = (np.sin(xx*RADIANS*sp_frq)+np.cos(yy*RADIANS*sp_frq + phi))
+        v = np.clip(v, 0,1)
+        image[i, :,:, rgbi] = np.floor(v*255)
+
+imageio.mimsave('./anim1.gif', image, duration=dt)
 
 
 #imageio.mimsave(exportname, frames, format='GIF', duration=5)
+
+# https://www.programcreek.com/python/example/104522/imageio.mimsave
